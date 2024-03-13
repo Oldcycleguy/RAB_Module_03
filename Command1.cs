6c08452047fcffa68acac42f5a87642ee853c217
Andy Webb <a.webb@calcinotto.co.uk>
2024-03-13 13:25:21 +0000

    Add project files.

---
+++ Command1.cs
@@ -0,0 +1,129 @@
+#region Namespaces
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+
+#endregion
+
+namespace RAB_Module_03
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command1 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // this is a variable for the Revit application
+            UIApplication uiapp = commandData.Application;
+
+            // this is a variable for the current Revit model
+            Document doc = uiapp.ActiveUIDocument.Document;
+
+            // Create instances of furniture class
+
+
+
+            return Result.Succeeded;
+        }
+
+        // set up the classes
+
+        // Furniture Type class
+        public class FurnType
+        {
+            public string Name { get; set; }
+            public string FamName { get; set; }
+            public string FamType { get; set;}
+
+            // class constructor
+            public FurnType(string _name, string _famname,  string _famtype) 
+            {
+            Name = _name;
+            FamName = _famname;
+            FamType = _famtype;
+            }
+        }
+
+        // Furniture set class
+        public class FurnSet
+        {
+            public string Set { get; set; }
+            public string Type { get; set; }
+            public List<FurnType> FurnTypeList { get; set; }
+            public FurnSet(string _set, string _type, List<FurnType> _furnTypes)
+            {
+                Set = _set;
+                Type = _type;
+                FurnTypeList = _furnTypes;
+            }
+
+            // method to count the furniture in each set
+            public int GetFurnitureCount() 
+            { 
+                return FurnTypeList.Count; 
+            }
+        }
+
+        // imported methods
+        private List<string[]> GetFurnitureTypes()
+        {
+            List<string[]> returnList = new List<string[]>();
+            returnList.Add(new string[] { "Furniture Name", "Revit Family Name", "Revit Family Type" });
+            returnList.Add(new string[] { "desk", "Desk", "60in x 30in" });
+            returnList.Add(new string[] { "task chair", "Chair-Task", "Chair-Task" });
+            returnList.Add(new string[] { "side chair", "Chair-Breuer", "Chair-Breuer" });
+            returnList.Add(new string[] { "bookcase", "Shelving", "96in x 12in x 84in" });
+            returnList.Add(new string[] { "loveseat", "Sofa", "54in" });
+            returnList.Add(new string[] { "teacher desk", "Table-Rectangular", "48in x 30in" });
+            returnList.Add(new string[] { "student desk", "Desk", "60in x 30in Student" });
+            returnList.Add(new string[] { "computer desk", "Table-Rectangular", "48in x 30in" });
+            returnList.Add(new string[] { "lab desk", "Table-Rectangular", "72in x 30in" });
+            returnList.Add(new string[] { "lounge chair", "Chair-Corbu", "Chair-Corbu" });
+            returnList.Add(new string[] { "coffee table", "Table-Coffee", "30in x 60in x 18in" });
+            returnList.Add(new string[] { "sofa", "Sofa-Corbu", "Sofa-Corbu" });
+            returnList.Add(new string[] { "dining table", "Table-Dining", "30in x 84in x 22in" });
+            returnList.Add(new string[] { "dining chair", "Chair-Breuer", "Chair-Breuer" });
+            returnList.Add(new string[] { "stool", "Chair-Task", "Chair-Task" });
+
+            return returnList;
+        }
+
+        private List<string[]> GetFurnitureSets()
+        {
+            List<string[]> returnList = new List<string[]>();
+            returnList.Add(new string[] { "Furniture Set", "Room Type", "Included Furniture" });
+            returnList.Add(new string[] { "A", "Office", "desk, task chair, side chair, bookcase" });
+            returnList.Add(new string[] { "A2", "Office", "desk, task chair, side chair, bookcase, loveseat" });
+            returnList.Add(new string[] { "B", "Classroom - Large", "teacher desk, task chair, student desk, student desk, student desk, student desk, student desk, student desk, student desk, student desk, student desk, student desk, student desk, student desk" });
+            returnList.Add(new string[] { "B2", "Classroom - Medium", "teacher desk, task chair, student desk, student desk, student desk, student desk, student desk, student desk, student desk, student desk" });
+            returnList.Add(new string[] { "C", "Computer Lab", "computer desk, computer desk, computer desk, computer desk, computer desk, computer desk, task chair, task chair, task chair, task chair, task chair, task chair" });
+            returnList.Add(new string[] { "D", "Lab", "teacher desk, task chair, lab desk, lab desk, lab desk, lab desk, lab desk, lab desk, lab desk, stool, stool, stool, stool, stool, stool, stool" });
+            returnList.Add(new string[] { "E", "Student Lounge", "lounge chair, lounge chair, lounge chair, sofa, coffee table, bookcase" });
+            returnList.Add(new string[] { "F", "Teacher's Lounge", "lounge chair, lounge chair, sofa, coffee table, dining table, dining chair, dining chair, dining chair, dining chair, bookcase" });
+            returnList.Add(new string[] { "G", "Waiting Room", "lounge chair, lounge chair, sofa, coffee table" });
+
+            return returnList;
+        }
+        internal static PushButtonData GetButtonData()
+        {
+            // use this method to define the properties for this command in the Revit ribbon
+            string buttonInternalName = "btnCommand1";
+            string buttonTitle = "Button 1";
+
+            ButtonDataClass myButtonData1 = new ButtonDataClass(
+                buttonInternalName,
+                buttonTitle,
+                MethodBase.GetCurrentMethod().DeclaringType?.FullName,
+                Properties.Resources.Blue_32,
+                Properties.Resources.Blue_16,
+                "This is a tooltip for Button 1");
+
+            return myButtonData1.Data;
+        }
+    }
+}
